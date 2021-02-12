62319a90aa381184d2f329b826403cea624b1e28
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-12 10:31:08 -0600

    fetching actions using router and express

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,12 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require(`./actions-model`);
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    Actions.get()
+        .then((action)=>{
+            res.status(200).json(action)
+        }) .catch(error=>res.status(400).json({message:`Error: ${error}`}))
+})
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -4,4 +4,14 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+//add actions routes
+const actionsRouter = require('./actions/actions-router');
+server.use(`/api/actions`,actionsRouter);
+
+//engage express with json
+server.use(express.json());
+server.get('/',(req,res)=>{
+    res.send(`Hello bubblegum, this is sprint 1 of unit 4!`)
+})
+
 module.exports = server;
